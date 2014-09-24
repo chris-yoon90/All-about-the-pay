0c8f388ba070a441d6360023c6b410a0fe3430d9
Chris Yoon <chris.yoon90@gmail.com>
2014-09-23 23:39:34 -0700

    landing-page design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def full_title(title) 
+		default_title = "All-About-The-Pay"
+		if(!title.empty?)
+			default_title += " | #{title}"
+		end
+		default_title
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
 
 end
