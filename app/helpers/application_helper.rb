e79981bdf682b5cf4f37e2980875038b43785be6
Chris Yoon <chris.yoon90@gmail.com>
2014-10-23 23:16:56 -0700

    Employee index page styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
 	WillPaginate::ViewHelpers.pagination_options[:next_label] = ">>"
 	WillPaginate::ViewHelpers.pagination_options[:previous_label] = "<<"
+	WillPaginate.per_page = 10
 
 	def full_title(title) 
 		default_title = "All-About-The-Pay"
